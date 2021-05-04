408016efa7475ee200d6c5e7d83ec2e6d8fc1e7e
Eric Chin <eric.chin@liferay.com>
2021-05-04 12:51:09 -0700

    Move factory to respective package

--- modules/forms-function-extension/src/main/java/com/liferay/forms/extension/factory/ArrayContainsFunctionFactory.java
+++ modules/forms-function-extension/src/main/java/com/liferay/forms/extension/factory/ArrayContainsFunctionFactory.java
@@ -1,7 +1,8 @@
-package com.liferay.forms.extension;
+package com.liferay.forms.extension.factory;
 
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunctionFactory;
+import com.liferay.forms.extension.ArrayContainsFunction;
 
 import org.osgi.service.component.annotations.Component;
 
